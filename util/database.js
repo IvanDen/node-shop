81650172a78d2ab622f7b3f6fdf356198a9d6c73
Дениско Иван Анатольевич <i.denisko@sigma-it.ru>
2022-11-23 21:56:15 +0100

    Initial commit

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'P@ssword'
+});
+
+module.exports = pool.promise();
